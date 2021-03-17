33cef3b84c358ded344bb756a3484d69373d9f08
백준용 <wnsdyd1015@pubg.com>
2021-03-17 18:02:14 +0900

    Add API 호출까지 완료

--- github-search/src/components/user-list/user-list.component.ts
+++ github-search/src/components/user-list/user-list.component.ts
@@ -4,15 +4,15 @@ import { IUser } from '@src/services/GithubAPIService';
 import { EState } from './user-list.state';
 
 @Component({
-    selector: 'app-user-list',
-    templateUrl: './user-list.component.html',
-    styleUrls: ['./user-list.component.scss'],
+  selector: 'app-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.scss'],
 })
 export class UserListComponent {
-    @Input() users: IUser[] = [];
-    @Input() state = EState.NeedUserName;
+  @Input() users: IUser[] = [];
+  @Input() state = EState.NeedUserName;
 
-    get EState() {
-        return EState;
-    }
+  get EState() {
+    return EState;
+  }
 }
--- github-search/src/services/GithubAPIService.ts
+++ github-search/src/services/GithubAPIService.ts
@@ -1,21 +1,29 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { ajax } from 'rxjs/ajax';
+import { pluck } from 'rxjs/operators';
 
 export interface IUser {
-    id: number
-    login: string
-    avatar_url: string
-    html_url: string
+  id: number;
+  login: string;
+  avatar_url: string;
+  html_url: string;
 }
 
 @Injectable({
-    providedIn: 'root',
+  providedIn: 'root',
 })
 export class GithubApiService {
-    static readonly BASE_URL = 'https://api.github.com';
+  static readonly BASE_URL = 'https://api.github.com';
 
-    // TODO: username을 받아 github api를 이용해 검색하는 로직 작성
-    searchUser(username: string): Observable<IUser[]> {
-        return of<IUser[]>([]);
-    }
+  constructor(private http: HttpClient) {}
+
+  // TODO: username을 받아 github api를 이용해 검색하는 로직 작성
+  searchUser(username: string): Observable<IUser[]> {
+    const userUrl = `/search/users?q=${username}`;
+    return this.http
+      .get<IUser[]>(GithubApiService.BASE_URL + userUrl)
+      .pipe(pluck('items'));
+  }
 }
