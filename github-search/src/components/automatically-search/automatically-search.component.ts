f4356981e1688b90cf08aeb2104fc8171f6cb9e4
백준용 <wnsdyd1015@pubg.com>
2021-03-26 09:54:14 +0900

    update

--- github-search/src/components/automatically-search/automatically-search.component.ts
+++ github-search/src/components/automatically-search/automatically-search.component.ts
@@ -18,22 +18,24 @@ export class AutomaticallySearchComponent implements OnInit, OnDestroy {
   users: IUser[] = [];
 
   // TODO: github api를 이용해 유저를 검색.
-  searchUser$: Observable<IUser[]> = this.githubApiService.searchUser('back');
+  searchUser$: Observable<IUser[]> = this.githubApiService.searchUser('ba');
 
   // TODO: search state를 변경시키는 스크림
   state$: Observable<EState>;
 
   constructor(private githubApiService: GithubApiService) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
+    // const changeQuery$: Observable<string> = this.username.valueChanges;
+
     // TODO: searchUser$, state$가 의도대로 동작하도록 초기화 필요.
-    /* this.state$.pipe(takeUntil(this.destroy$)).subscribe((state) => {
+    /*   this.state$.pipe(takeUntil(this.destroy$)).subscribe((state) => {
+      // this.state = state;
       console.log(state);
     }); */
 
     this.searchUser$.pipe(takeUntil(this.destroy$)).subscribe(
       (users) => {
-        console.log(users);
         this.users = users;
         this.state = users.length === 0 ? EState.NoResult : EState.Success;
       },
@@ -43,7 +45,7 @@ export class AutomaticallySearchComponent implements OnInit, OnDestroy {
     );
   }
 
-  ngOnDestroy() {
+  ngOnDestroy(): void {
     this.destroy$.next();
     this.destroy$.complete();
   }
