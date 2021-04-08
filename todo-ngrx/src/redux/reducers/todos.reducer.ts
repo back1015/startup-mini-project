501cbb23e5807bdacbc0bacb145317831a044bd7
BLUEHOLE\wnsdyd1015 <back1015@gmail.com>
2021-04-08 18:10:53 +0900

    toogleDoneStatus 추가

--- todo-ngrx/src/redux/reducers/todos.reducer.ts
+++ todo-ngrx/src/redux/reducers/todos.reducer.ts
@@ -27,12 +27,18 @@ const reducer = createReducer(
   on(deleteTodo, (state, { id }) => ({
     ...state,
     todoItems: state.todoItems.filter((item) => item.id !== id),
-  }))
-  /*  on(toggleDoneSatus, (state, { id }) => ({
+  })),
+  on(toggleDoneSatus, (state, { id }) => ({
     ...state,
-    // 상태 값을 변경 해야되는데...
-    todoItems: state.todoItems.map((value) => {}),
-  })) */
+    // 상태 값을 변경 해야되는데..
+    // 현재 상태 값을 바로 변경 하면 문제가 발생된다. 항상 새로운 값으로 반환해주자.
+    todoItems: state.todoItems.map((todo) => {
+      if (todo.id === id) {
+        todo.done = !todo.done;
+      }
+      return todo;
+    }),
+  }))
 );
 
 export function todoReducer(state = initialState, action: Action) {
